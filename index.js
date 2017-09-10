5aec8796ea9bc15d356cd1a4f83233e16969081f
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-10 19:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,3 +33,7 @@ function prependKitten(name) {
 function removeLastKitten(name) {
   return kittens.slice(0, kittens.length - 1)
 }
+
+function removeFirstKitten(name) {
+  return kittens.slice(1)
+}
